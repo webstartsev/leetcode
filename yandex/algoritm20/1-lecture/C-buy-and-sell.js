65758ba01b1a936ad7692c0bda86031a2549ebcf
Sergey Startsev <sergey@webstartsev.ru>
2022-09-07 21:37:55 +0300

    WIP: C-buy-and-sell

--- yandex/algoritm20/1-lecture/C-buy-and-sell.js
+++ yandex/algoritm20/1-lecture/C-buy-and-sell.js
@@ -1,43 +1,53 @@
 const buyAndSell = (str) => {
   const prices = str.split(" ").map((item) => Number(item));
-  let prevMinPosition = 0;
-  let prevMaxPosition = 0;
+  let bank = 1;
 
-  let minPosition = 0;
-  let maxPosition = 0;
+  let minPosition = -1;
+  let maxPosition = -1;
 
-  let findNew = false;
+  const profit = {};
 
-  for (let i = 1; i < prices.length; i++) {
-    if (prices[i] < prices[minPosition]) {
-      //   if (findNew) {
-      //     prevMinPosition = minPosition;
-      //     prevMaxPosition = maxPosition;
-      //   }
-
+  for (let i = 0; i < prices.length; i++) {
+    if (
+      prices[i] <= bank &&
+      (prices[i] < prices[minPosition] || !prices[minPosition])
+    ) {
       minPosition = i;
+    } else if (prices[i] > prices[minPosition]) {
       maxPosition = i;
-      findNew = true;
+    }
+
+    if (minPosition !== maxPosition && minPosition < maxPosition) {
+      const maxProfit = prices[maxPosition] - prices[minPosition];
+      profit[maxProfit] = [minPosition, maxPosition];
     }
   }
 
-  for (let i = minPosition; i < prices.length; i++) {
-    if (prices[i] > prices[maxPosition]) {
-      maxPosition = i;
-    }
-  }
-
-  if (minPosition === maxPosition) {
+  const bestDaysForOffer = profit[Object.keys(profit).pop()];
+  if (!bestDaysForOffer) {
     return "0 0";
   }
 
-  return `${minPosition + 1} ${maxPosition + 1}`;
+  return `${bestDaysForOffer[0] + 1} ${bestDaysForOffer[1] + 1}`;
 };
 
-// console.log(buyAndSell("10 3 5 3 11 9"));
-// console.log(buyAndSell("5 5 5 5"));
-// console.log(buyAndSell("5 5 5 4"));
-// console.log(buyAndSell("5"));
-// console.log(buyAndSell("1 2"));
-
+console.log(buyAndSell("501 1000 2000 502 600")); // 2 5
+console.log(buyAndSell("10 3 5 3 11 9")); // 2 5
+console.log(buyAndSell("5 5 5 5")); // 0 0
+console.log(buyAndSell("5 5 5 4")); // 0 0
+console.log(buyAndSell("5")); // 0 0
+console.log(buyAndSell("1 2")); // 1 2
 console.log(buyAndSell("4 5 1 1")); // 1 2
+console.log(buyAndSell("4 10 1 2")); // 1 2
+console.log(buyAndSell("4 5 1 2")); // 3 4
+console.log(buyAndSell("1 2 3 4")); // 1 4
+console.log(buyAndSell("5 5 5 5")); // 0 0
+console.log(buyAndSell("5 4 3 2 1")); // 0 0
+console.log(buyAndSell("1 10 5 3")); // 1 2
+console.log(buyAndSell("1 10 2 15")); // 1 4
+console.log(buyAndSell("1 10 2 5")); // 1 2
+console.log(buyAndSell("5")); // 0 0
+console.log(buyAndSell("1000 1001")); // 1 2
+console.log(buyAndSell("1500 2000 4001")); // 0 0
+console.log(buyAndSell("1500 2000 4001 500 2000 100 5")); // 4 5
+console.log(buyAndSell("1 10 8 1 10 8 100 1 1 3 2 4 1")); // 1 7
